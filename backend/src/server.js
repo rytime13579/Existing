2a2ce3825eb8b4386822226a7f5d5383b398bb87
Ryan Martin <cmart216@uncc.edu>
2025-10-13 13:54:20 -0400

    added chat page
    still need to add contact functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(express.json()); // req.body
 app.use(cookieParser());
 
 app.use("/api/auth/", authRoutes,);
-app.use("/api/message/", messageRoutes,);
+app.use("/api/messages/", messageRoutes,);
 
 // make ready for deployment
 
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,45 @@
+import {create} from 'zustand';
+import {axiosInstance} from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+    allContacts: [],
+    chats: [],
+    messages: [],
+    activeTab: "chats",
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    
+    setActiveTab: (tab) => set({activeTab:tab}),
+    setSelectedUser: (selectedUser) => set({selectedUser}),
+
+    getAllContacts: async() => {
+        set ({ isUsersLoading: true});
+        try {
+            // get all contacts from the back end using an api rquests
+            const res = await axiosInstance.get("/messages/contacts");
+            set ({ allContatcs: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in get all contacts", error);
+        } finally {
+            set({ isUsersLoading: false});
+        }   
+
+    },
+    getMyChatPartners: async() => {
+        set ({isUsersLoading: true});
+        try {
+            // get all chat partners from back end api
+            const res = await axiosInstance.get("/messages/chats");
+            set ({chats: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in get chat partners", error);
+        } finally {
+            set ({ isUsersLoading: false});
+        }
+    }
+
+}));
